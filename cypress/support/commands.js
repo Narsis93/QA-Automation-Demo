226b38e2e7885dee346b95ed898455f726e36ea4
Narsis93 <106112306+Narsis93@users.noreply.github.com>
2025-11-26 22:05:13 +0300

    Create commands.js

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('login', () => {
+  cy.visit('/');
+  cy.get('#user-name').type('standard_user');
+  cy.get('#password').type('secret_sauce');
+  cy.get('#login-button').click();
+});
